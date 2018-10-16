76695806ea2496b62b6e5583e2660d443faea694
mamaki <mamaki@MC149A19-L.pstcc.edu>
2018-10-16 10:49:47 -0400

    Hello to all

---
+++ src/abc/Hello.java
@@ -0,0 +1,12 @@
+/* Programer : Michael Maki
+ * Date : 10/16/2018
+ * Purpose : demo : GitHub
+ */
+package abc;
+
+public class Hello {
+    public static void main(String[]args)
+    {
+        System.out.println("Hello");
+    }
+}
